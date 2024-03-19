b9be8083b9917ec5c75d9d4348794877e1d5fe15
Z0mbr0 <163907528+Z0mbr0@users.noreply.github.com>
2024-03-19 16:26:24 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const config = {
+  challenge: true, // Set to true if you want to enable password protection.
+  users: {
+    // You can add multiple users by doing username: 'password'.
+    zombro01: '†††',
+    CJ: 'notrn',
+    Frank: '0301',
+    Marshall: 'Singer16',
+    chris: 'big96',
+    username: 'password',
+    username: 'password',
+    username: 'password',
+    Guest: 'zyxwvutsrqponmlkjihgfedcba',
+  },
+  routes: true, // Change this to false if you just want to host a bare server.
+  local: false, // Change this to false to disable local assets.
+}
+export default config
